2a0ef129f2c9b6679872d53ca42357ad201c2406
Mayank Mehta <mayankmehta880@gmail.com>
2024-10-31 03:57:33 +0530

    completed home page css for displaying data

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -38,7 +38,7 @@ const Home = () => {
                 <p>{item.name + " - " + item.symbol}</p>
               </div>
               <p>{currency.symbol} {item.current_price.toLocaleString()}</p>
-              <p>{Math.floor(item.market_cap_change_percentage_24h*100)/100}</p>
+              <p className={item.market_cap_change_percentage_24h > 0?"green":"red"}>{Math.floor(item.market_cap_change_percentage_24h*100)/100}</p>
               <p className='market-cap'>{currency.symbol} {item.market_cap.toLocaleString()}</p>
             </div>
           ))
