aaa524e4abf214711306d59242e78888e2149137
Mayank Mehta <mayankmehta880@gmail.com>
2024-11-01 06:02:29 +0530

    complete links feature

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import "./Home.css";
 import { CoinContext } from '../../context/CoinContext';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 
@@ -39,8 +40,6 @@ const Home = () => {
                   {allCoin.map((item,index)=>(<option key={index} value={item.name} />))}
                 </datalist>
 
-
-
                 <button type='submit'>Search</button>
             </form> 
         </div>
@@ -52,9 +51,9 @@ const Home = () => {
             <p style={{textAlign:"center"}}>24H Change</p>
             <p className='market-cap'>Market Cap</p>
         </div>
-        {
+        { 
           displayCoin.slice(0,10).map((item,index)=>(
-            <div className='table-layout' key={index}>
+            <Link to={`/coin/${item.id}`} className='table-layout' key={index}>
               <p>{item.market_cap_rank}</p>
               <div>
                 <img src={item.image} alt="" />
@@ -63,7 +62,7 @@ const Home = () => {
               <p>{currency.symbol} {item.current_price.toLocaleString()}</p>
               <p className={item.market_cap_change_percentage_24h > 0?"green":"red"}>{Math.floor(item.market_cap_change_percentage_24h*100)/100}</p>
               <p className='market-cap'>{currency.symbol} {item.market_cap.toLocaleString()}</p>
-            </div>
+            </Link>
           ))
         }
       </div>
